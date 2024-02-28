1a4fe3cd4bcde76e399816e478ae5a2303b9623e
Daniele206 <danieledeieso2004@gmail.com>
2024-02-28 17:38:35 +0100

    [add] full game "pari o dispari" to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,31 @@ const playGame = document.getElementById('play-game');
 const selectPari = document.getElementById('select-pari');
 const selectDispari = document.getElementById('select-dispari');
 
+// Element-none
+let select;
+
 // Input
 const wordInput = document.getElementById('word-input');
+const numInput = document.getElementById('number-input');
 
 // Button
 const plaiBtn = document.getElementById('pali-btn');
 const playGameBtn = document.getElementById('play-game-btn');
 const pariBtn = document.getElementById('pari-btn');
 const dispariBtn = document.getElementById('dispari-btn');
+const playNumberBtn = document.getElementById('play-number-btn');
 
 // Output
 const testWordOutput = document.getElementById('test-word-output');
+const errorNumOutput = document.getElementById('error-num');
+const numPcOutput = document.getElementById('num-pc-output');
+const winOutput = document.getElementById('win');
 
 // Functions
 function paliTest(word){
   const wordSplit = word.split('');
-  const wordReverse = (wordSplit.reverse().join(''));
+  const wordSplitreverse = wordSplit.reverse();
+  const wordReverse = (wordSplitreverse.join(''));
   if(word === ''){
     return 'Parola mancante "Inserire una parola palindorma"';
   }else if(word === wordReverse){
@@ -38,6 +47,41 @@ function palyGame(test){
     plaiBtn.classList.add('d-none');
   };
 };
+
+function testWin(num){
+  let numValid;
+  let winCondition;
+  if (num > 5 || num < 1){
+    errorNumOutput.innerHTML = 'Inserisci un numero valido';
+    errorNumOutput.classList.remove('d-none');
+    numPcOutput.classList.add('d-none');
+    winOutput.classList.add('d-none');
+  }else if(isNaN(num)){
+    errorNumOutput.innerHTML = 'Inserisci un numero';
+    errorNumOutput.classList.remove('d-none');
+    numPcOutput.classList.add('d-none');
+    winOutput.classList.add('d-none');
+  }else{
+    errorNumOutput.classList.add('d-none');
+    numPcOutput.classList.remove('d-none');
+    winOutput.classList.remove('d-none');
+    const numpc = Math.ceil(Math.random() * 5);
+    numPcOutput.innerHTML = `Il pc ha scelto ${numpc}`;
+    numValid = num + numpc;
+
+    if(numValid % 2 === 0){
+      winCondition = 'pari';
+    }else{
+      winCondition = 'dispari';
+    };
+
+    if(winCondition === select){
+      winOutput.innerHTML = `La somma dei dumeri é ${numValid} che é ${winCondition} quindi hai vinto!`;
+    }else{
+      winOutput.innerHTML = `La somma dei dumeri é ${numValid} che é ${winCondition} quindi ha vinto il pc.`
+    };
+  }
+}
 
 //---------------------------------------------------------------------
 
@@ -62,10 +106,20 @@ pariBtn.addEventListener('click', function(){
   startGame.classList.add('d-none');
   playGame.classList.remove('d-none');
   selectPari.classList.remove('d-none');
+  select = 'pari';
 });
 
 dispariBtn.addEventListener('click', function(){
   startGame.classList.add('d-none');
   playGame.classList.remove('d-none');
   selectDispari.classList.remove('d-none');
+  select = 'dispari';
 });
+
+playNumberBtn.addEventListener('click', function(){
+  const numTest = parseInt(numInput.value);
+  testWin(numTest);
+  console.log(testWin(numTest));
+
+  numInput.value = '';
+})
