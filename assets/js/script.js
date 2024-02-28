36cc13c9565333afdd8f56ad994cbd447b204983
Daniele206 <danieledeieso2004@gmail.com>
2024-02-28 16:09:36 +0100

    [add] test-aprola to HTML and play-game-btn to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,30 @@ const wordInput = document.getElementById('word-input');
 
 // Button
 const plaiBtn = document.getElementById('pali-btn');
+const playGameBtn = document.getElementById('play-game-btn');
 
 // Output
+const testWordOutput = document.getElementById('test-word-output');
 
 // Functions
 function paliTest(word){
   const wordSplit = word.split('');
-  console.log(wordSplit);
   const wordReverse = (wordSplit.reverse().join(''));
-  console.log(wordReverse);
-  if(word === wordReverse){
-    return 'La parola é palindorma'
+  if(word === ''){
+    return 'Parola mancante "Inserire una parola palindorma"';
+  }else if(word === wordReverse){
+    return 'La parola é  palindorma';
   }else{
-    return 'La parola non é palindroma'
+    return 'La parola non é palindroma';
+  };
+};
+
+function palyGame(test){
+  if(test === 'La parola é  palindorma'){
+    playGameBtn.classList.remove('d-none');
+    //-----
+    wordInput.classList.add('d-none');
+    plaiBtn.classList.add('d-none');
   }
 }
 
@@ -25,7 +36,8 @@ function paliTest(word){
 
 plaiBtn.addEventListener('click', function(){
   const wordTest = wordInput.value
-  console.log(wordTest);
   paliTest(wordTest);
-  console.log(paliTest(wordTest));
+  testWordOutput.innerHTML = paliTest(wordTest);
+
+  palyGame(paliTest(wordTest));
 })
